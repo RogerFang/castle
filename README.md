ced762fe3153a3bbbedb803612e6d5bfcec004d4
ken <ken@whenling.com>
2016-05-31 00:34:45 +0800

    Thymeleaf

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# castle
-castle的目标是打造一个高性能、高扩展性的java开发平台，适合于开发各种互联网软件产品。
+# Castle
+Castle的目标是打造一个高性能、高扩展性的java开发平台，适合于开发各种互联网软件产品。
 
 ## 特点
 Consumer和Provider分离，可选择地使用第三方服务管理框架（默认使用dubbo）。
@@ -13,4 +13,5 @@ Consumer和Provider分离，可选择地使用第三方服务管理框架（默�
 * mongodb（文档数据库）
 * neo4j（图形数据库）
 * groovy-template（碎片化模板）
+* thymeleaf
 * extjs6（js组件框架）
--- castle-framework/castle-web/src/main/java/com/whenling/castle/framework/web/view/ViewConfigBean.java
+++ castle-framework/castle-web/src/main/java/com/whenling/castle/framework/web/view/ViewConfigBean.java
@@ -1,14 +1,29 @@
 package com.whenling.castle.framework.web.view;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.groovy.GroovyMarkupConfigurer;
 import org.springframework.web.servlet.view.groovy.GroovyMarkupViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import com.whenling.castle.framework.web.view.groovy.CastleBaseTemplate;
 
+/**
+ * 视图配置
+ * 
+ * @author ken
+ *
+ */
 @Configuration
 public class ViewConfigBean {
+
+	@Autowired
+	private ServletContext servletContext;
 
 	@Bean
 	public GroovyMarkupViewResolver groovyMarkupViewResolver() {
@@ -28,6 +43,32 @@ public class ViewConfigBean {
 		configurer.setCacheTemplates(false);
 		configurer.setDeclarationEncoding("UTF-8");
 		return configurer;
+	}
+
+	@Bean
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setOrder(1);
+		resolver.setViewNames(new String[] { "*.html", "*.xhtml" });
+		return resolver;
+	}
+
+	//SpringResourceTemplateResolver:classpath
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		// templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
 	}
 
 }
